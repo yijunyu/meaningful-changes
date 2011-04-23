b19f81b28cab9aec3a158538790b5b4b85bd1147
yy66 <yy66@open.ac.uk>
2011-04-23 23:39:41 +0000

    + import the current draft of the paper
    git-svn-id: file://localhost/var/www/svn/mct/trunk@8 b39b4f57-c5e0-45cc-95f1-0f3fb6671ad7

---
+++ paper/yu11ase/code/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld 
+{
+ static String hello = "Hello"; // beginning
+ static String world = "world"; // ending
+ static public void main(String args[]) {
+  System.out.println(hello + ", " + world + "!");
+ }
+}
---
+++ paper/yu11ase/code/HelloWorld2.java
@@ -0,0 +1,10 @@
+public class HelloWorld 
+{
+ static String world = "world"; // ending
+
+ static String hello = "Hello"; // beginning
+ public  static void main(String args[]) {
+  System.out.println  (hello + ", " 
+     + world + "!");
+ }
+}
