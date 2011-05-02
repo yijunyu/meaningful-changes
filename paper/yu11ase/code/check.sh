626e3da2229e4ff1c2f7699e526162bab9b9cbc6
yy66 <yy66@open.ac.uk>
2011-05-02 08:32:58 +0000

    + abstract submitted
    git-svn-id: file://localhost/var/www/svn/mct/trunk@38 b39b4f57-c5e0-45cc-95f1-0f3fb6671ad7

--- paper/yu11ase/code/check.sh
+++ paper/yu11ase/code/check.sh
@@ -4,3 +4,6 @@ cat -n HelloWorld-2.java
 cat -n HelloWorld-2.diff
 /usr/bin/time ../../../cvs/ldiff.pl -w -o diff HelloWorld-1.java HelloWorld-2.java > HelloWorld-2.ldiff
 cat -n HelloWorld-2.ldiff
+/usr/bin/time ../../../bin.`uname -s -m | sed 's/ /_/'`/javac  HelloWorld-2.java > HelloWorld-2.norm
+cat HelloWorld-1.java HelloWorld-2.java > HelloWorld-2.clone
+/usr/bin/time ../../../bin.`uname -s -m | sed 's/ /_/'`/javac  HelloWorld-2.clone > HelloWorld-2.mct
