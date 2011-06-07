890dc368d71af0e828ca5854a1086b4089e6afcb
yy66 <yy66@open.ac.uk>
2011-06-07 00:26:50 +0000

    - some error to be fixed with the java + comment parser
    git-svn-id: file://localhost/var/www/svn/mct/trunk@116 b39b4f57-c5e0-45cc-95f1-0f3fb6671ad7

---
+++ test.sh
@@ -0,0 +1,9 @@
+if [ ! -f test.txt ]; then
+	grep -l "@generated NOT" cvs/gmf/J/[A-F]*.java > test.txt
+	grep -l "@generated NOT" cvs/gmf/J/[G-M]*.java >> test.txt
+	grep -l "@generated NOT" cvs/gmf/J/[N-Z]*.java >> test.txt
+fi
+for f in `cat test.txt`; do
+ echo $f
+ scripts/mct-mdsd -comment $f
+done
